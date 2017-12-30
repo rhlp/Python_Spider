c1e9763af11acc0cb16fcc8b397f51718d20f0d5
rhlp <renhl1991@163.com>
2017-12-30 23:00:25 +0800

    tencent - CrawlSpider版本 和 下载中间件设置

--- day09/Tencent/Tencent/items.py
+++ day09/Tencent/Tencent/items.py
@@ -9,6 +9,37 @@ import scrapy
 
 
 class TencentItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    """
+        保存列表页的item
+    """
+    # 职位名
+    position_name = scrapy.Field()
+    # 职位详情链接
+    position_link = scrapy.Field()
+    # 职位类别
+    position_type = scrapy.Field()
+    # 招聘人数
+    people_number = scrapy.Field()
+    # 工作地点
+    work_location = scrapy.Field()
+    # 发布时间
+    publish_times = scrapy.Field()
+
+    # 数据源
+    source = scrapy.Field()
+    # 抓取数据的时间
+    crawl_time = scrapy.Field()
+
+class PositionItem(scrapy.Item):
+    """
+        保存详情页的item
+    """
+    # 工作职责
+    position_zhize = scrapy.Field()
+    # 工作要求
+    position_yaoqiu = scrapy.Field()
+    # 数据源
+    source = scrapy.Field()
+    # 抓取数据的时间
+    crawl_time = scrapy.Field()
+
--- day09/Tencent/Tencent/middlewares.py
+++ day09/Tencent/Tencent/middlewares.py
@@ -5,52 +5,20 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-from scrapy import signals
+from settings import USER_AGENT_LIST
 
+import random
 
-class TencentSpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
+# 注意：　中间件中不需要return
+class UserAgentMiddleware(object):
+    def process_request(self, request, spider):
+        user_agent = random.choice(USER_AGENT_LIST)
+        request.headers["User-Agent"] = user_agent
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Response, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+# class ProxyMiddleware(object):
+#     def process_request(self, request, spider):
+#         # 1. 免费代理
+#         #proxy = "http://120.26.167.140:16816"
+#         # 2. 验证代理
+#         proxy = "http://mr_mao_hacker:sffqry9r@120.26.167.140:16816"
+#         request.meta['proxy'] = proxy
